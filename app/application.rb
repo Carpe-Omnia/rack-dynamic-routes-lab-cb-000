ae446e511fd8a492a8a9b165c76122a05116c65b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-04 20:21:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,17 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-      resp.write "items"
+      item_name = req.path.split("/items/").last
+      if Item.find_by_name(item_name) != nil
+        resp.write "#{Item.find_by_name(item_name).price}"
+      else
+        resp.status = 400
+        resp.write "We don't have that item"
+      end
     else
       resp.status = 404
     end
     resp.finish
   end
+
 end
