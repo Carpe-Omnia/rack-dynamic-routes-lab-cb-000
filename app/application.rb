3177daad2be83628d2cfc30080ed268ad562e5ca
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-04 20:08:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,9 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    
+    if req.path.match(/items/)
+      resp.write "items"
+    else 
+      resp.status = 404 
+    end 
+  end       
